994516d625056eb6d4af602c2b8b22f028597937
Miguel Flores <mflores9203@bths.edu>
2023-09-29 22:12:34 -0400

    Construct a clock

---
+++ app.js
@@ -0,0 +1,7 @@
+
+
+const secondsHandle = document.querySelector('.handle__seconds')
+const minutesHandle = document.querySelector('.handle__minutes')
+const hoursHandle = document.querySelector('.handle__hours')
+
+console.log(secondsHandle)
